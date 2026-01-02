b8a4d801e617921c0b1409c7a53b9780abf6f6f7
Mariano138 <agueromariano138@gmail.com>
2026-01-02 18:53:06 -0300

    1.0.0

--- api/weather.ts
+++ api/weather.ts
@@ -17,14 +17,34 @@ export default async function handler(req, res) {
     }
   }
 
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
-      city
-    )}&appid=${process.env.WEATHER_KEY}&units=metric&lang=es`
-  );
+  try {
+    const currentRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
+        city
+      )}&appid=${process.env.WEATHER_KEY}&units=metric&lang=es`
+    );
 
-  const data = await response.json();
+    const forecastRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(
+        city
+      )}&appid=${process.env.WEATHER_KEY}&units=metric&lang=es`
+    );
 
-  cache.set(key, { data, time: Date.now() });
-  res.status(200).json(data);
+    const current = await currentRes.json();
+    const forecastRaw = await forecastRes.json();
+
+    const forecastDaily = forecastRaw.list.filter((item) => item.dt_txt.includes('12:00:00'));
+
+    const responseData = {
+      current,
+      forecast: forecastDaily,
+    };
+
+    cache.set(key, { data: responseData, time: Date.now() });
+
+    return res.status(200).json(responseData);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Weather service failed' });
+  }
 }
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,12 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import useFetch from 'hooks/useFetch';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function Home() {
+  const { fetchWeather, city, setCity, isLoading, weather, error } = useFetch();
+
   return (
     <View>
-      <Text>Weather App</Text>
+      <TextInput placeholder="Search" value={city} onChangeText={setCity} />
+      <Button title="Search" onPress={() => fetchWeather()} />
+
+      {isLoading && <Text>Cargando...</Text>}
+
+      {weather && (
+        <View>
+          <Text>{weather.name}</Text>
+          <Text>{weather.main.temp}</Text>
+          <Text>{weather.weather[0].description}</Text>
+          <Image
+            style={styles.icon}
+            source={{ uri: `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png` }}
+          />
+        </View>
+      )}
+      {error && <Text>{error}</Text>}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  icon: {
+    width: 50,
+    height: 50,
+  },
+});
